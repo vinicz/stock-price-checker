e3daca549f4e13877c9254b101112bc4952c87b3
David Viniczay <viniczd@gmail.com>
2025-02-11 17:06:14 +0100

    feat(main): Updated the main module and added basic http request logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './services/database/database.module';
+import { StockPriceModule } from './domain/stock-price/stock-price.module';
+import { V1Module } from './interface/v1/v1.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    StockPriceModule,
+    V1Module,
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    morgan((tokens, req, res) =>
+      [
+        tokens['remote-addr'](req, res),
+        tokens.date(req, res, 'clf'),
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'),
+        tokens.referrer(req, res),
+        tokens['user-agent'](req, res),
+        '-',
+        (req as any).user ? (req as any).user.clientId : 'no_user',
+        '-',
+        tokens['response-time'](req, res),
+        'ms',
+      ].join(' '),
+    ),
+  );
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
